da54dbe2786f18c52e06007d5ab27651c543b335
ioloizou <yiannisloizou@gmail.com>
2024-04-20 00:03:56 +0200

    added diff robot to rviz

---
+++ mobile_robot/launch/rviz_launch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from simple_launch import SimpleLauncher
+
+def generate_launch_description():
+    sl = SimpleLauncher()
+
+	# Start the rviz node
+    sl.rviz()
+    
+    # We get the robot type to generate description
+    sl.robot_state_publisher('mobile_robot', 'diff_robot.urdf', 'urdf')
+
+    # Start the joint state publisher node
+    sl.joint_state_publisher(use_gui=True)
+
+    return sl.launch_description()
