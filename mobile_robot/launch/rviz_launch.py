b54f6f28b3cdeaac482f993b03f18b0a22cc567d
ioloizou <yiannisloizou@gmail.com>
2024-04-27 21:56:03 +0200

    added gazebo launch

--- mobile_robot/launch/rviz_launch.py
+++ mobile_robot/launch/rviz_launch.py
@@ -5,6 +5,7 @@ def generate_launch_description():
     sl = SimpleLauncher()
 
     sl.declare_arg('use_slider', True, description = 'Use slider for publishing linear and angular velocity')
+    sl.declare_arg('gazebo', False, description = 'Open Gazebo')
     sl.declare_arg('v', 0., description = 'Set constant linear velocity')
     sl.declare_arg('w', 0., description = 'Set cosnstant angular velocity')
 
@@ -17,6 +18,9 @@ def generate_launch_description():
     # Start the slider publisher node
     with sl.group(if_condition=sl.arg('use_slider')):
         sl.node('slider_publisher', 'slider_publisher', name ='Twist', arguments = [sl.find('mobile_robot', 'Twist.yaml')])
+
+    with sl.group(if_condition=sl.arg('gazebo')):
+        sl.include('mobile_robot','gazebo_launch.py')
 
     # Start differentail control node
     sl.node('mobile_robot','diff_control', output='screen', parameters = {'use_slider': sl.arg('use_slider'), 'v': sl.arg('v'), 'w':sl.arg('w')})
